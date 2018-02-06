43042583f6652579e9bb571b10f4b62f9a2fa92f
Tyler Rick <tyler@tylerrick.com>
2018-02-06 15:17:20 -0800

    Actually export MaphilightComponent in maphilight.module.ts

--- src/app/maphilight/maphilight.module.ts
+++ src/app/maphilight/maphilight.module.ts
@@ -2,6 +2,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaphilightComponent } from './maphilight.component';
+export { MaphilightComponent } from './maphilight.component';
 
 @NgModule({
   imports: [
